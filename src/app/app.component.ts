81a32aa63207179bb968be4027dc056460572a2e
little-by <kotarzhang@outlook.com>
2017-12-02 22:18:12 +0800

    modified:   src/app/app.component.css 	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heros';
+  hero = 'Windstorm';
 }
